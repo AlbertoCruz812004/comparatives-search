1fe067e728563fe340e1e33bc698078b87aa266c
Jose Alberto Cruz Martinez <albertocruz8133@hotmail.com>
2024-10-04 19:10:02 -0600

    add: dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { Button } from "@nextui-org/button";
 import Search from "./components/Search";
+import Sun from "./icon/Sun";
+import Moon from "./icon/Moon";
+import useDarkTheme from "./useDarkTheme";
 
 export default function App() {
+
+  const { toogle, changeTheme } = useDarkTheme();
+
   return (
-    <section className="py-5">
-      <h1 className="text-3xl font-bold text-center px-5">
-        Discover Comparatives: Your Ultimate Search Engine
-      </h1>
-      <Search />
-    </section>
+    <>
+      <header className="py-5 px-6 flex justify-between">
+        <span className="flex gap-2 h-10 items-center"><img className="w-11 p-1 bg-slate-700 rounded-full" src="./../public/white_on_black-transformed-remove.png" alt="" /> CompariSearch</span>
+        <Button onClick={changeTheme} className="p-2" isIconOnly size="sm">
+          {
+            toogle ? <Moon className="w-10 h-10" /> : <Sun className="w-10 h-10" />
+          }
+        </Button>
+      </header>
+      <section className="py-5">
+        <h1 className="text-3xl font-bold text-center px-5">
+          Discover Comparatives: Your Ultimate Search Engine
+        </h1>
+        <Search />
+      </section>
+    </>
   )
 }
